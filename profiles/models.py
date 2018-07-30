ce8c6c0bf613c1ac205150f835d62c52bc4eff08
Adiela Abishua <adiela.abishua@gmail.com>
2018-07-30 16:01:51 +0300

    Created Profile model that is related to User

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,19 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
+
+class Profile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(blank=True, null=True)
+    bio = models.TextField(max_length=200, blank=True)
+
+
+def create_user_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = Profile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_user_profile, sender=User)
