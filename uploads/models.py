de56b39d4c3ba96f6bcde215a4c29c8bac2f6a3f
Adiela Abishua <adiela.abishua@gmail.com>
2018-07-30 17:08:15 +0300

    Created Image Model

--- uploads/models.py
+++ uploads/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Image(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=50, blank=True)
+    image_caption = models.TextField(blank=True)
+    poster = models.ForeignKey(User)
