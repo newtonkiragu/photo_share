00e7d9a7f8879df5697c4d1b9d3345bf8cbdfafb
Adiela Abishua <adiela.abishua@gmail.com>
2018-07-30 17:25:30 +0300

    configured storage and serving of user uploaded files

--- photo_share/settings.py
+++ photo_share/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.contrib.messages.context_processors.media',
             ],
         },
     },
@@ -125,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Location for saving user uploaded media
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
