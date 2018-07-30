e86c406876e2b2e60d07088d3f0b2f9db4d17aae
Adiela Abishua <adiela.abishua@gmail.com>
2018-07-30 17:47:26 +0300

    Created image model methods

--- uploads/models.py
+++ uploads/models.py
@@ -9,3 +9,19 @@ class Image(models.Model):
     image_name = models.CharField(max_length=50, blank=True)
     image_caption = models.TextField(blank=True)
     poster = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.id
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_caption(self, caption):
+        self.update(caption=caption)
+
+    @classmethod
+    def get_image_by_id(cls, n):
+        Image.objects.get(id=n)
