f6ad0f403d4eb232728b1a0fe2c4137d579cba94
Adiela Abishua <adiela.abishua@gmail.com>
2018-07-31 14:35:22 +0300

    Added image form to home page

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm
+from uploads.forms import ImageForm
 
 
 def home(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        image_form = ImageForm(request.POST, request.FILES)
+        if image_form.is_valid():
+            image = image_form.save(commit=False)
+            image.user = request.user
+            image.save()
+    else:
+        image_form = ImageForm()
+    return render(request, 'home.html', {"image_form": image_form})
 
 
 @login_required(login_url='/accounts/login/')
--- uploads/views.py
+++ uploads/views.py
@@ -1,14 +1,1 @@
 from django.shortcuts import render
-from .forms import ImageForm
-
-
-def upload_image(request):
-    if request.method == 'POST':
-        image_form = ImageForm(request.POST, request.FILES)
-        if image_form.is_valid():
-            image = image_form.save(commit=False)
-            image.user = request.user
-            image.save()
-    else:
-        image_form = ImageForm()
-    return render(request, 'upload.html', {"image_form": image_form})
