8b2c2b52e8d57427b2cc636a9763c0f1d185d2bc
Adiela Abishua <adiela.abishua@gmail.com>
2018-07-31 12:23:44 +0300

    Created edit account view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm
 
-# Create your views here.
+
+@login_required(login_url='/accounts/login/')
+def edit_account(request):
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, request.FILES)
+        if profile_form.is_valid():
+            profile = profile_form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+    else:
+        profile_form = ProfileForm()
+    return render(request, 'account/edit.html', {"profile_form": profile_form})
