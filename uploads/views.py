bc17f027040de773342397aa2efb3a3d8978d0d9
Adiela Abishua <adiela.abishua@gmail.com>
2018-07-31 13:58:39 +0300

    Created Upload Image Page

--- uploads/views.py
+++ uploads/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import ImageForm
 
-# Create your views here.
+
+def upload_image(request):
+    if request.method == 'POST':
+        image_form = ImageForm(request.POST, request.FILES)
+        if image_form.is_valid():
+            image = image_form.save(commit=False)
+            image.user = request.user
+            image.save()
+    else:
+        image_form = ImageForm()
+    return render(request, 'upload.html', {"image_form": image_form})
