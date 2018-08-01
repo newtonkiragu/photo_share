b56d78fe567165ba3e92e99d8862f3d013627781
Adiela Abishua <adiela.abishua@gmail.com>
2018-08-01 21:22:29 +0300

    Created follow user and post image urls

--- photo_app/urls.py
+++ photo_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('accounts/edit/', views.account_edit, name='account_edit'),
     path('accounts/profile/', views.account_profile, name='account_profile'),
     path('<username>/', views.user_profile, name='user_profile'),
+    path('follow/<username>/', views.follow_user, name='follow_user'),
+    path('image/post/', views.image_post, name='image_post'),
 ]
