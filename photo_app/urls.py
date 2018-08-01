2b4e36fa54d847018605eb95d87e13ed24958efb
Adiela Abishua <adiela.abishua@gmail.com>
2018-08-01 07:29:31 +0300

    Created user_profile url

--- photo_app/urls.py
+++ photo_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/edit/', views.account_edit, name='account_edit'),
     path('accounts/profile/', views.account_profile, name='account_profile'),
+    path('<username>/', views.user_profile, name='user_profile'),
 ]
