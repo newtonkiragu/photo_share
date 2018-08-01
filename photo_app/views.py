d09bb8bdeb19bac0ca323acb8fc0c5c9ddb8a3d4
Adiela Abishua <adiela.abishua@gmail.com>
2018-08-01 21:24:38 +0300

    Created post image and follow views

--- photo_app/views.py
+++ photo_app/views.py
@@ -2,11 +2,17 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from friendship.models import Follow
 from .models import Image
 from .forms import ProfileForm, ImageForm
 
 
 def home(request):
+
+    return render(request, 'home.html',)
+
+
+def image_post(request):
 
     if request.method == 'POST':
         image_form = ImageForm(request.POST, request.FILES)
@@ -17,7 +23,7 @@ def home(request):
             messages.success(request, 'Your picture has successfully been uploaded.')
     else:
         image_form = ImageForm()
-    return render(request, 'home.html', {"image_form": image_form})
+    return render(request, 'image_post.html', {"image_form": image_form})
 
 
 @login_required(login_url='/accounts/login/')
@@ -37,11 +43,24 @@ def account_edit(request):
 @login_required(login_url='/accounts/login/')
 def account_profile(request):
     user = request.user
+    followers = Follow.objects.followers(request.user)
+    following = Follow.objects.followers(request.user)
+    followers_number = len(followers)
+    following_number = len(following)
     images = Image.objects.filter(poster=user)
-    return render(request, 'account/profile.html', {"user": user, "images": images})
+    return render(request, 'account/profile.html', {"user": user, "images": images, "followers": followers,
+                                                    "following": following, "followers_number": followers_number,
+                                                    "following_number": following_number})
 
 
 def user_profile(request, username):
     user = User.objects.get(username=username)
     images = Image.objects.filter(poster=user)
     return render(request, 'account/profile.html', {"user": user, "images": images})
+
+
+@login_required(login_url='/accounts/login/')
+def follow_user(request, username):
+    other_user = User.objects.get(username=username)
+    Follow.objects.add_follower(request.user, other_user)
+    return render(request, '')
